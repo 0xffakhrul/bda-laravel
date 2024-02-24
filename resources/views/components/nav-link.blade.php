465501669641e6ac02ea1d8697aaecd2fa81551b
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2024-02-24 17:25:50 +0800

    improve nav ui

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,11 +1,15 @@
 @props(['active'])
 
 @php
-$classes = ($active ?? false)
-            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 dark:border-indigo-600 text-sm font-medium leading-5 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out';
+    $linkClasses = $active ? 'block rounded-lg bg-gray-100 text-rose-700' : '';
+    $svgClasses = $active ? 'text-rose-700' : 'text-gray-500';
 @endphp
 
-<a {{ $attributes->merge(['class' => $classes]) }}>
-    {{ $slot }}
+<a {{ $attributes->merge(['class' => $linkClasses]) }}>
+    <div class="items-center p-2 hover:bg-gray-100 flex rounded-lg">
+        <svg class="w-5 h-5 transition duration-75 group-hover:text-gray-900 {{ $svgClasses }}" aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+            {!! $slot !!}
+        </svg>
+    </div>
 </a>
