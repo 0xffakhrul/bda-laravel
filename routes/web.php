a1499121c362da7f81f3e01fb7acffcf425078b9
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2024-02-24 17:27:30 +0800

    add more routes

--- routes/web.php
+++ routes/web.php
@@ -37,14 +37,21 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
     Route::get('/admin/users', [AdminUserController::class, 'index'])->name('admin.users.index');
     Route::get('/admin/users/create', [AdminUserController::class, 'create'])->name('admin.users.create');
+    Route::get('/admin/users/{id}/edit', [AdminUserController::class, 'edit'])->name('admin.users.edit');
+    Route::put('/admin/users/{id}', [AdminUserController::class, 'update'])->name('admin.users.update');
     Route::post('/admin/users', [AdminUserController::class, 'store'])->name('admin.users.store');
     Route::get('/admin/appointments', [AdminAppointmentController::class, 'index'])->name('admin.appointments.index');
+    Route::get('/admin/appointments/{id}', [AdminAppointmentController::class, 'show'])->name('admin.appointments.show');
+    Route::get('/admin/appointments/{id}/edit', [AdminAppointmentController::class, 'edit'])->name('admin.appointments.edit');
+    Route::put('/admin/appointments/{id}', [AdminAppointmentController::class, 'update'])->name('admin.appointments.update');
+    Route::delete('/admin/appointments/{id}', [AdminAppointmentController::class, 'destroy'])->name('admin.appointments.destroy');
 });
 
 Route::middleware(['auth', 'role:donor'])->group(function () {
     Route::get('/donor/dashboard', [DonorDashboardController::class, 'index'])->name('donor.dashboard');
     Route::get('/donor/appointments', [DonorAppointmentController::class, 'index'])->name('donor.appointments.index');
     Route::get('/donor/appointments/create', [DonorAppointmentController::class, 'create'])->name('donor.appointments.create');
+    Route::get('/donor/appointments/{id}/edit', [DonorAppointmentController::class, 'edit'])->name('donor.appointments.edit');
     Route::post('/donor/appointments', [DonorAppointmentController::class, 'store'])->name('donor.appointments.store');
 });
 
