dc4f08b09cfe66612df873b3ebdcb3e6b06f028e
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2024-02-24 17:26:09 +0800

    complete create appointments page for donor. now using timeslot

--- resources/views/donor/appointments/create.blade.php
+++ resources/views/donor/appointments/create.blade.php
@@ -1,29 +1,43 @@
 <x-app-layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="py-8">
+        <div class="max-w-screen-2xl mx-auto sm:px-6 lg:px-8">
+            {{ Breadcrumbs::render('donor.appointments.create') }}
             <h2 class="text-3xl font-bold pb-5">Schedule New Appointment</h2>
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <form action="{{ route('donor.appointments.store') }}" method="POST" class="space-y-4">
                         @csrf
-                        <div class="grid grid-cols-2 gap-6 mb-5">
+                        <div class="grid grid-cols-2 gap-6">
                             <div class="space-y-2">
                                 <label class="block" for="">Date</label>
-                                <input name="date" class="w-full border border-gray-300 rounded-md" type="date">
+                                <input name="date"
+                                    class="w-full transition duration-75 border border-slate-300 rounded-md focus:outline-none focus:border-rose-500 focus:ring-rose-700"
+                                    type="date">
                             </div>
                             <div class="space-y-2">
                                 <label class="block" for="">Time</label>
-                                <input name="time" class="w-full border border-gray-300 rounded-md" type="time">
+                                <select name="time"
+                                    class="w-full transition duration-75 border border-slate-300 rounded-md focus:outline-none focus:border-rose-500 focus:ring-rose-700">
+                                    <option value="08:00">8:00 AM</option>
+                                    <option value="09:00">9:00 AM</option>
+                                    <option value="10:00">10:00 AM</option>
+                                    <option value="11:00">11:00 AM</option>
+                                    <option value="12:00">12:00 PM</option>
+                                    <option value="14:00">2:00 PM</option>
+                                    <option value="15:00">3:00 PM</option>
+                                    <option value="16:00">4:00 PM</option>
+                                    <option value="17:00">5:00 PM</option>
+                                </select>
                             </div>
                         </div>
                         <div class="space-y-2">
                             <label class="block" for="">Notes</label>
-                            <textarea name="notes" class="w-full border border-gray-300 rounded-md" id="" cols="10" rows="10"></textarea>
+                            <textarea name="notes"
+                                class="w-full transition duration-75 border border-slate-300 rounded-md focus:outline-none focus:border-rose-500 focus:ring-rose-700"
+                                id="" cols="10" rows="10"></textarea>
                         </div>
-                        {{-- <input name="notes" class="w-full border border-gray-300 rounded-md" type="notes"> --}}
-
                         <div class="">
-                            <button class="bg-red-400 px-4 py-2 rounded-md font-bold text-gray-50"
+                            <button class="bg-rose-700 px-4 py-2 rounded-md font-bold text-gray-50"
                                 type="submit">Submit</button>
                         </div>
                     </form>
